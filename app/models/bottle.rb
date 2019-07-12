457487500ff9e64053fe8e334f873376e5b1f5f0
Jenniekerf <jenniekerf@gmail.com>
2019-07-12 18:17:21 -0400

    added comments to bottle model

--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -6,4 +6,8 @@ class Bottle < ApplicationRecord
   has_many :commentors, through: :comments, source: :commentor
 end
 
-#source: :bottle
+#source: :bottle (try this again if not working)
+#add a scope method for price:
+#cheap = where price<20
+#moderate = where price is btw 20-40
+#fancy = where price>40
