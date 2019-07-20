e988403ceef7d997aa633db0217d69828e457563
Jenniekerf <jenniekerf@gmail.com>
2019-07-20 10:57:50 -0400

    finished omniauth login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,15 +7,13 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash = request.env["omniauth.auth"]
-      #binding.pry
        @user = User.find_or_create_by_omniauth(auth_hash)
-       @user.save
        session[:user_id] = @user.id
 
        redirect_to bottles_path
     else
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
+    @user = User.find_by(:email => params[:email])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
 
       redirect_to bottles_path
